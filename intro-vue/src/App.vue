d714daf58e256d55c212b7881e29cb28ebbd0539
setiawanswan <setiawan.tumilin@gmail.com>
2020-12-06 20:59:58 +0700

    conditional in vue

---
+++ intro-vue/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <input type="number" v-model="age">
+  <div v-if="age < 18">
+    Usia anda dibawah umur 18 tahun    
+  </div>
+  <div v-else-if="age > 25">
+    Anda sudah terlalu tua
+  </div>
+  <div v-else>
+    Usia anda mencukupi
+  </div>
+</div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+export default{
+  data(){
+    return {
+      age: 21
+    }
+  }
+}
+
+</script>
+
