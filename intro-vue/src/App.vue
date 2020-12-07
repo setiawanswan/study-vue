a3e300425b632028fe79571063066a24fa22d432
setiawanswan <setiawan.tumilin@gmail.com>
2020-12-07 15:12:08 +0700

    props in vue

--- intro-vue/src/App.vue
+++ intro-vue/src/App.vue
@@ -1,20 +1,6 @@
 <template>
 <div id="app">
-  <div>
-    <input type="number" v-model="age">
-    <div v-if="age < 18">
-      Usia anda dibawah umur 18 tahun    
-    </div>
-    <div v-else-if="age > 25">
-      Anda sudah terlalu tua
-    </div>
-    <div v-else>
-      Usia anda mencukupi
-    </div>
-  </div>
-  <div>
-    <Todos  v-bind:todos="todos"/>
-  </div>
+  <Todos  v-bind:todos="todos"/>
 </div>
 </template>
 
@@ -29,7 +15,6 @@ export default{
   },
   data(){
     return {
-      age: 21,
       todos: [
         {
         "userId": 1,
